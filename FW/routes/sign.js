6e3b9c3fe25139acee524584b4cc467c865ae206
gordon <flash.gordon77176@gmail.com>
2015-10-08 17:46:39 +0200

    MVC page admin done

--- FW/routes/sign.js
+++ FW/routes/sign.js
@@ -2,5 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('sign-in', function(req, res) {
+	res.render('/sign/sign-in');
+});
 
+router.get('sign-up', function(req, res){
+	res.render('sign/sign-up');
+});
 module.exports = router;
